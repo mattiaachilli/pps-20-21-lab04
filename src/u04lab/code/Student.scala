50004c16a4f3ade5cb31ef07420e391f27cae05c
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-03-22 20:57:14 +0100

    add tests in Student.scala

--- src/u04lab/code/Student.scala
+++ src/u04lab/code/Student.scala
@@ -1,7 +1,7 @@
 package u04lab.code
 
 import Lists._
-import u04lab.code.Lists.List.{Cons, nil} // import custom List type (not the one in Scala stdlib)
+import u04lab.code.Lists.List.{Cons, Nil, append, map, contains} // import custom List type (not the one in Scala stdlib)
 
 trait Student {
   def name: String
@@ -22,13 +22,46 @@ object Student {
   private case class StudentImpl(override val name: String,
                                 override val year: Int) extends Student {
 
-    private var _courses: List[Course] = List.Nil()
+    private var _courses: List[Course] = Nil()
 
-    override def enrolling(courseToAdd: Course*): Unit = courseToAdd foreach(course => _courses = List.append(_courses, Cons(course, List.Nil())))
+    override def enrolling(courseToAdd: Course*): Unit = courseToAdd foreach(course => _courses = append(_courses, Cons(course, Nil())))
 
-    override def courses: List[String] = List.map(_courses)(_.name)
+    override def courses: List[String] = map(_courses)(_.name)
 
-    override def hasTeacher(teacher: String): Boolean = List.contains(List.map(_courses)(_.teacher))(teacher)
+    override def hasTeacher(teacher: String): Boolean = contains(map(_courses)(_.teacher))(teacher)
+  }
+}
+
+class StudentCourseTest {
+  import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue}
+  import org.junit.jupiter.api.{BeforeEach, Test}
+
+  val cPPS: Course = Course("PPS","Viroli")
+  val cPCD: Course = Course("PCD","Ricci")
+  val cSDR: Course = Course("SDR","D'Angelo")
+  val cASW: Course = Course("AWS", "Mirri")
+  val s1: Student = Student("mattia",2015)
+
+  @BeforeEach
+  def initCourses(): Unit = {
+    s1.enrolling(cPPS)
+    s1.enrolling(cPCD)
+  }
+
+  @Test def testCourses(): Unit = {
+    assertEquals(Cons("PPS",Cons("PCD", Nil())), s1.courses)
+  }
+
+  @Test def testHasTeacher(): Unit = {
+    assertTrue(s1.hasTeacher("Viroli"))
+    assertTrue(s1.hasTeacher("Ricci"))
+  }
+
+  @Test def testMultipleEnrolling(): Unit = {
+    s1.enrolling(cSDR, cASW)
+    assertEquals(Cons("PPS",Cons("PCD", Cons("SDR", Cons("AWS", Nil())))), s1.courses)
+    assertTrue(s1.hasTeacher("D'Angelo"))
+    assertTrue(s1.hasTeacher("Mirri"))
   }
 }
 
@@ -39,6 +72,7 @@ object Course {
                                  override val teacher: String) extends Course
 }
 
+/*
 object Try extends App {
   val cPPS = Course("PPS","Viroli")
   val cPCD = Course("PCD","Ricci")
@@ -54,7 +88,7 @@ object Try extends App {
   s3.enrolling(cSDR)
   println(s1.courses, s2.courses, s3.courses) // (Cons(PCD,Cons(PPS,Nil())),Cons(PPS,Nil()),Cons(SDR,Cons(PCD,Cons(PPS,Nil()))))
   println(s1.hasTeacher("Ricci")) // true
-}
+}*/
 
 /** Hints:
   * - simply implement Course, e.g. with a case class
--- test/u04lab/code/StudentCourseTest.scala
+++ test/u04lab/code/StudentCourseTest.scala
@@ -4,32 +4,4 @@ import org.junit.jupiter.api.{BeforeEach, Test}
 import org.junit.jupiter.api.Assertions._
 import u04lab.code.Lists.List._
 
-class StudentCourseTest {
-  val cPPS: Course = Course("PPS","Viroli")
-  val cPCD: Course = Course("PCD","Ricci")
-  val cSDR = Course("SDR","D'Angelo")
-  val cASW = Course("AWS", "Mirri")
-  val s1: Student = Student("mattia",2015)
 
-  @BeforeEach
-  def initCourses(): Unit = {
-    s1.enrolling(cPPS)
-    s1.enrolling(cPCD)
-  }
-
-  @Test def testCourses(): Unit = {
-    assertEquals(Cons("PPS",Cons("PCD", Nil())), s1.courses)
-  }
-
-  @Test def testHasTeacher(): Unit = {
-    assertTrue(s1.hasTeacher("Viroli"))
-    assertTrue(s1.hasTeacher("Ricci"))
-  }
-
-  @Test def testMultipleEnrolling(): Unit = {
-    s1.enrolling(cSDR, cASW)
-    assertEquals(Cons("PPS",Cons("PCD", Cons("SDR", Cons("AWS", Nil())))), s1.courses)
-    assertTrue(s1.hasTeacher("D'Angelo"))
-    assertTrue(s1.hasTeacher("Mirri"))
-  }
-}
